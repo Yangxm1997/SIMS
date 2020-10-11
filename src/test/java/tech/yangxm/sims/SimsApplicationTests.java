6109563e4764985717ae042ae96f0f9c11114fdf
杨先明 <yangxianming1997@gmail.com>
2020-10-11 14:00:50 +0800

    init

---
+++ src/test/java/tech/yangxm/sims/SimsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.yangxm.sims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
